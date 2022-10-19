bd78744cd896c5ed1661baa4845ea4b665b2c9e1
Mike Sifanele <masilive.sifanele@gmail.com>
2022-10-19 21:04:58 +0200

    added ticks

---
+++ Data/Tick.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanicSurfer.Data
+{
+    public struct Tick
+    {
+        public DateTime Timestamp { get; set; }
+        public float Bid { get; set; }
+        public bool IsVerified { get; set; } = true;
+        public Tick(string[] data)
+        {
+            Timestamp = Convert.ToDateTime(data[0]);
+            Bid = Convert.ToSingle(data[1]);
+        }
+        public Tick(DateTime timestamp)
+        {
+            Timestamp = timestamp;
+            Bid = 0f;
+            IsVerified = false;
+        }
+    }
+}
---
+++ Extensions/Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanicSurfer.Extensions
+{
+    public static class Global
+    {
+        public static string Title = "This is a title";
+    }
+}
